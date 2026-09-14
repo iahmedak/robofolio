2fb62241ccdb028bb7dbaa791d3f8f8e981c6dfa
iahmedak <iahmedak@users.noreply.github.com>
2026-09-15 03:43:54 +0530

    fix: add node types for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ base }) => ({
-  // If deploying to GitHub Pages, use '/robofolio/'.
-  // If deploying to Cloudflare Pages or a custom domain, use '/'.
-  base: process.env.NODE_ENV === 'production' && !process.env.CLOUDFLARE ? "/robofolio/" : "/",
+export default defineConfig({
+  base: "/robofolio/",
   plugins: [react(), tailwindcss()],
-}));
+});
